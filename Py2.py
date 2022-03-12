86552cd413a2d0de3839c8706ab4dd13e744231d
Zhenkun Guo <1608229093@qq.com>
2022-03-12 21:46:04 +0800

    入门2

---
+++ Py2.py
@@ -0,0 +1,36 @@
+##float
+#from decimal import Decimal
+#print(Decimal('1.1')+Decimal('2.2'))
+
+##bool
+#a=True # 1
+#b=False # 0
+#print(a,type(a))
+#print(b,type(b))
+
+##bool值可以转成整数用来计算
+#print(a+1)
+#print(b+1).
+
+
+#str1='人生苦短，我用python'
+#str2="人生苦短，我用python"
+#str3='''人生苦短，
+#我用python'''
+#str4="""人生苦短，
+#我用python"""
+#print(str1,type(str1))
+#print(str2,type(str2))
+#print(str3,type(str3))
+#print(str4,type(str4))
+
+
+
+##
+#name='abc'
+#age=20
+#print('ccc'+name+'ddd'+str(age)+'ss') #强制类型转换
+## print('ccc'+name+'ddd'+age+'ss') #error - 不能将int与str连接
+##str转换成int类型时若str为小数串则会报错
+##在float类型转换成int类型后会发生截断
+##将str转换为int类型时此时str必须为数字串，同理，转为float也相同
